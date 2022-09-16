eaaa448b87133370b7418ec6b12d8fabed8507c0
JORDAN HENRIQUE MOREIRA ZANONI <52969396+zanonijordan@users.noreply.github.com>
2022-09-16 14:53:48 -0300

    add new index contacts and App.jsx

--- src/index.js
+++ src/index.js
@@ -1,33 +1,40 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
+function Card(props) {
+  console.log(props);
+    return (
+        <div>
+            <h2>Beyonce</h2>
+            <img
+                src="https://blackhistorywall.files.wordpress.com/2010/02/picture-device-independent-bitmap-119.jpg"
+                alt="avatar_img"/>
+            <p>+123 456 789</p>
+            <p>b@beyonce.com</p>
+        </div>
+    )
+}
+
+
 ReactDOM.render(
   <div>
     <h1>My Contacts</h1>
-
-    <h2>Beyonce</h2>
-    <img
-      src="https://blackhistorywall.files.wordpress.com/2010/02/picture-device-independent-bitmap-119.jpg"
-      alt="avatar_img"
-    />
-    <p>+123 456 789</p>
-    <p>b@beyonce.com</p>
-
-    <h2>Jack Bauer</h2>
-    <img
-      src="https://pbs.twimg.com/profile_images/625247595825246208/X3XLea04_400x400.jpg"
-      alt="avatar_img"
-    />
-    <p>+987 654 321</p>
-    <p>jack@nowhere.com</p>
-
-    <h2>Chuck Norris</h2>
-    <img
-      src="https://i.pinimg.com/originals/e3/94/47/e39447de921955826b1e498ccf9a39af.png"
-      alt="avatar_img"
-    />
-    <p>+918 372 574</p>
-    <p>gmail@chucknorris.com</p>
+      <Card name="byonce" />
   </div>,
   document.getElementById("root")
 );
+/* <h2>Jack Bauer</h2>
+<img
+  src="https://pbs.twimg.com/profile_images/625247595825246208/X3XLea04_400x400.jpg"
+  alt="avatar_img"
+/>
+<p>+987 654 321</p>
+<p>jack@nowhere.com</p>
+
+<h2>Chuck Norris</h2>
+<img
+  src="https://i.pinimg.com/originals/e3/94/47/e39447de921955826b1e498ccf9a39af.png"
+  alt="avatar_img"
+/>
+<p>+918 372 574</p>
+<p>gmail@chucknorris.com</p> */
