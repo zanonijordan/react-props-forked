7460202790323cb0408d5780c354b3e62c903347
JORDAN HENRIQUE MOREIRA ZANONI <52969396+zanonijordan@users.noreply.github.com>
2022-09-16 14:54:48 -0300

    ok

--- src/index.js
+++ src/index.js
@@ -1,40 +1,13 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import App from "./components/App";
 
-function Card(props) {
-  console.log(props);
-    return (
-        <div>
-            <h2>Beyonce</h2>
-            <img
-                src="https://blackhistorywall.files.wordpress.com/2010/02/picture-device-independent-bitmap-119.jpg"
-                alt="avatar_img"/>
-            <p>+123 456 789</p>
-            <p>b@beyonce.com</p>
-        </div>
-    )
-}
+ReactDOM.render(<App />, document.getElementById("root"));
 
-
-ReactDOM.render(
-  <div>
-    <h1>My Contacts</h1>
-      <Card name="byonce" />
-  </div>,
-  document.getElementById("root")
-);
-/* <h2>Jack Bauer</h2>
-<img
-  src="https://pbs.twimg.com/profile_images/625247595825246208/X3XLea04_400x400.jpg"
-  alt="avatar_img"
-/>
-<p>+987 654 321</p>
-<p>jack@nowhere.com</p>
-
-<h2>Chuck Norris</h2>
-<img
-  src="https://i.pinimg.com/originals/e3/94/47/e39447de921955826b1e498ccf9a39af.png"
-  alt="avatar_img"
-/>
-<p>+918 372 574</p>
-<p>gmail@chucknorris.com</p> */
+//1. Apply CSS styles to App.jsx component
+//to match the appearance on the completed app:
+//https://c6fkx.csb.app/
+//2. Extract the contact card as a reusable Card component.
+//3. Use props to render the default Beyonce contact card
+//so the Card component can be reused for other contacts.
+//4. Import the contacts.js file to create card components.
